d559f21828af0caa49538cf7c39618a56fb1e55b
Ista Zahn <istazahn@gmail.com>
2018-06-14 13:35:27 -0400

    add exercise1

---
+++ Notes.R
@@ -0,0 +1,9 @@
+## Ex 1.
+## This exercise will give you a chance to
+## learn the R / Rstudio interface
+##
+## 1. Add 2 and 2
+
+## 2. Calculate the square root of 10
+
+## 3. Find R documentation "An introduction to R"
