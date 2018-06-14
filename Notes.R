d1f2e1976b98a2f778f41e17dc2ac43b5065a096
riverjaaa <jaaa667@yahoo.com>
2018-06-14 13:57:48 -0400

    These are my answers to the workshop questions.

--- Notes.R
+++ Notes.R
@@ -3,7 +3,16 @@
 ## learn the R / Rstudio interface
 ##
 ## 1. Add 2 and 2
+2 + 2
+#answer 4
+
 
 ## 2. Calculate the square root of 10
+sqrt(10)
+#answer 3.162278
+
 
 ## 3. Find R documentation "An introduction to R"
+
+                                        #its here:    https://cran.r-project.org/doc/manuals/r-release/R-intro.pdf
+help.start()
