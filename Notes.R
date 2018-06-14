24235a637d27202fe43626469c73d9add18b2deb
AntonXue <antonxue1@gmail.com>
2018-06-14 13:52:35 -0400

    squashed changes

--- Notes.R
+++ Notes.R
@@ -3,7 +3,15 @@
 ## learn the R / Rstudio interface
 ##
 ## 1. Add 2 and 2
+2 + 2
 
 ## 2. Calculate the square root of 10
+sqrt(10)
 
 ## 3. Find R documentation "An introduction to R"
+# install.packages("curl")
+library(curl)
+url <- "https://cran.r-project.org/doc/manuals/r-release/R-intro.pdf"
+curl_download(url=url, dest="~/Desktop/R-intro.pdf")
+
+
